2e213728c255c86211cb90b8f1d4bcd414a33401
bSuslu <51022680+bSuslu@users.noreply.github.com>
2025-03-24 15:22:07 +0300

    added simple mono singleton

---
+++ Assets/_Game/Singleton/Singleton.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+namespace TB_RPG_2D.Singleton
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindFirstObjectByType<T>();
+                    if (_instance == null)
+                    {
+                        GameObject obj = new GameObject(typeof(T).Name);
+                        _instance = obj.AddComponent<T>();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        [SerializeField] private bool _doNotDestroyOnLoad = false;
+
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+                if (_doNotDestroyOnLoad)
+                {
+                    DontDestroyOnLoad(gameObject);
+                }
+            }
+            else if (_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
