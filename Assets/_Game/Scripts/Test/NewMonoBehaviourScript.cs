1769dabff386fc039523ab7f2afdd39908383a62
bSuslu <51022680+bSuslu@users.noreply.github.com>
2025-03-24 13:56:06 +0300

    tested and adjusted code editor settings
    added initial scene
    set root namespace
    ignored some default folder namespace providers

---
+++ Assets/_Game/Scripts/Test/NewMonoBehaviourScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace TB_RPG_2D.Test
+{
+    public class NewMonoBehaviourScript : MonoBehaviour
+    {
+        void Start()
+        {
+            Debug.Log("Started NewMonoBehaviourScript");
+        
+        }
+
+        void Update()
+        {
+            Debug.Log("Updated NewMonoBehaviourScript");
+        }
+    }
+}
