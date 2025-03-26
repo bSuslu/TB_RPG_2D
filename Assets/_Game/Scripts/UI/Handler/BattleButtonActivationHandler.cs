39c9f800f8a4d09f44837c06959e74f0b823d255
bSuslu <51022680+bSuslu@users.noreply.github.com>
2025-03-26 16:22:46 +0300

    added enter battle button with activation logic

---
+++ Assets/_Game/Scripts/UI/Handler/BattleButtonActivationHandler.cs
@@ -0,0 +1,30 @@
+using TB_RPG_2D.EventSystem;
+using TB_RPG_2D.EventSystem.Events;
+using TB_RPG_2D.UI.Core;
+
+namespace TB_RPG_2D.UI.Handler
+{
+    public class BattleButtonActivationHandler : UIEntity
+    {
+        private EventBinding<HeroSelectionCompleteStateChangedEvent> _heroSelectionCompleteStateChangedEventBinding;
+        public override void Init()
+        {
+            base.Init();
+            
+            _heroSelectionCompleteStateChangedEventBinding = new EventBinding<HeroSelectionCompleteStateChangedEvent>(OnHeroSelectionCompleteStateChanged);
+            EventBus<HeroSelectionCompleteStateChangedEvent>.Subscribe(_heroSelectionCompleteStateChangedEventBinding);
+        }
+
+        private void OnHeroSelectionCompleteStateChanged(HeroSelectionCompleteStateChangedEvent heroSelectionCompleteStateChangedEvent)
+        {
+            gameObject.SetActive(heroSelectionCompleteStateChangedEvent.IsComplete);
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            EventBus<HeroSelectionCompleteStateChangedEvent>.Unsubscribe(
+                _heroSelectionCompleteStateChangedEventBinding);
+        }
+    }
+}
