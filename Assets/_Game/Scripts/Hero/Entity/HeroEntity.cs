e3228558e29f1c99b6c15127859bb4f826bed342
bSuslu <51022680+bSuslu@users.noreply.github.com>
2025-03-27 17:24:45 +0300

    aded health bar prefab

--- Assets/_Game/Scripts/Hero/Entity/HeroEntity.cs
+++ Assets/_Game/Scripts/Hero/Entity/HeroEntity.cs
@@ -1,15 +1,19 @@
 using TB_RPG_2D.Hero.Data;
+using TB_RPG_2D.UI.Health;
 using UnityEngine;
 
 namespace TB_RPG_2D.Hero.Entity
 {
     public class HeroEntity : MonoBehaviour
     {
+        [SerializeField] private HealthBar _healthBar;
         [SerializeField] private SpriteRenderer _spriteRenderer;
         
         public void SetData(HeroData heroData)
         {
             _spriteRenderer.sprite = heroData.Icon;
+            _healthBar.SetMaxHealth(heroData.Health);
+            _healthBar.SetCurrentHealth(heroData.Health);
         }
     }
 }
